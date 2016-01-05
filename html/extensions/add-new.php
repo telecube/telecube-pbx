f5568c5084aeae773e66323b80b158a3de21d77c
telecube <john@telecube.com.au>
2016-01-05 22:45:13 +0700

    tidying up

--- html/extensions/add-new.php
+++ html/extensions/add-new.php
@@ -3,11 +3,9 @@ require("../init.php");
 
 $pass = $Common->random_password(12);
 
-$q = "insert into sip_devices (name, context, secret, host, type, `call-limit`, insecure, defaultuser) values (?,?,?,?,?,?,?,?);";
-$data = array($_POST['ext'], "voip-extensions", $pass, "dynamic", "friend", 0, "port,invite", $_POST['ext']);
+$q = "insert into sip_devices (name, context, secret, host, nat, type, `call-limit`, insecure, defaultuser) values (?,?,?,?,?,?,?,?,?);";
+$data = array($_POST['ext'], "voip-extensions", $pass, "dynamic", "force_rport,comedia", "friend", 0, "port,invite", $_POST['ext']);
 $res = $Db->pdo_query($q,$data,$dbPDO);
-//print_r($res);
-
 
 header("Location: /extensions/")
 ?>
