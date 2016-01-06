1c1dded3a561fb13e7f30b2382a4cd66acca21b9
telecube <john@telecube.com.au>
2016-01-06 09:31:24 +0700

    tidying up

--- html/update/git-update.php
+++ html/update/git-update.php
@@ -8,13 +8,13 @@ if(file_exists("/opt/telecube-pbx")){
 
 	echo "<pre>";
 
-	exec("/usr/bin/git -C /opt/telecube-pbx/ status", $output, $return_var);
+	exec("sudo /usr/bin/git -C /opt/telecube-pbx/ status", $output, $return_var);
 	print_r($output);
 
-	exec("/usr/bin/rsync -av --delete /opt/telecube-pbx/agi-bin /var/lib/asterisk/", $output, $return_var);
+	exec("sudo /usr/bin/rsync -av --delete /opt/telecube-pbx/agi-bin /var/lib/asterisk/", $output, $return_var);
 	print_r($output);
 
-	exec("/usr/bin/rsync -av --delete /opt/telecube-pbx/html/ /var/www/html/", $output, $return_var);
+	exec("sudo /usr/bin/rsync -av --delete /opt/telecube-pbx/html/ /var/www/html/", $output, $return_var);
 	print_r($output);
 
 	echo "<pre>";
