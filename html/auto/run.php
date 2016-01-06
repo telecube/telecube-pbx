14f7e49c4acd247a7d0172b872e7cc94dda9ddda
telecube <john@telecube.com.au>
2016-01-06 18:22:00 +0700

    tidying up

--- html/auto/run.php
+++ html/auto/run.php
@@ -1,5 +1,8 @@
 <?php
-require(__DIR__."/../init.php");
+// this script is run via phpcli so no server vars are available to it
+
+echo "hello \n";
+//require(__DIR__."/../init.php");
 
 // set a random api update check time
 //$update_check_min = $Common->get_set_pref();
