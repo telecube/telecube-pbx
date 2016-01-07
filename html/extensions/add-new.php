f3b174c34639e120ccb8c66a460827d1ff9d742b
telecube <john@telecube.com.au>
2016-01-07 12:37:13 +0700

    security update fixing the insecure= field

--- html/extensions/add-new.php
+++ html/extensions/add-new.php
@@ -4,7 +4,7 @@ require("../init.php");
 $pass = $Common->random_password(12);
 
 $q = "insert into sip_devices (name, context, secret, host, nat, type, `call-limit`, insecure, defaultuser, bar_int, bar_13, bar_fixed, bar_mobile) values (?,?,?,?,?,?,?,?,?,?,?,?,?);";
-$data = array($_POST['ext'], "voip-extensions", $pass, "dynamic", "force_rport,comedia", "friend", 0, "port,invite", $_POST['ext'], "n", "n", "n", "n");
+$data = array($_POST['ext'], "voip-extensions", $pass, "dynamic", "force_rport,comedia", "friend", 0, "port", $_POST['ext'], "n", "n", "n", "n");
 $res = $Db->pdo_query($q,$data,$dbPDO);
 
 header("Location: /extensions/")
