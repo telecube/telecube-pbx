fed66b1475abeedbd9141c17c334fa8329c4fd51
telecube <john@telecube.com.au>
2016-01-12 03:43:01 +0700

    tidying up

--- html/index.php
+++ html/index.php
@@ -1,6 +1,25 @@
 <?php
 require("init.php");
 
+function get_server_cpu_usage(){
+
+    $load = sys_getloadavg();
+    return $load[0];
+
+}
+
+function get_server_memory_usage(){
+
+    $free = shell_exec('free');
+    $free = (string)trim($free);
+    $free_arr = explode("\n", $free);
+    $mem = explode(" ", $free_arr[1]);
+    $mem = array_filter($mem);
+    $mem = array_merge($mem);
+    $memory_usage = $mem[2]/$mem[1]*100;
+
+    return $memory_usage;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,7 +36,11 @@ require("init.php");
 
 		<h1>Telecube Cloud PBX</h1>
 		
-		<?php echo date("H:i:s");?>
+		<?php echo "<p>".date("H:i:s")."</p>";?>
+
+		<?php echo "<p>".get_server_cpu_usage()."</p>";?>
+
+		<?php echo "<p>".round(get_server_memory_usage(),2)."</p>";?>
 
 		</div>
 
