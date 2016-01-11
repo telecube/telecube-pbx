5c08a06eba6a892f1fd463854ae4c69736ea5aa9
telecube <john@telecube.com.au>
2016-01-12 03:03:48 +0700

    setting up to load minimal modules

---
+++ updates/sys/update-12.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "loading codec modules"
+
+/bin/echo "load => codec_a_mu.so" >> /etc/asterisk/modules.conf
+/bin/echo "load => codec_adpcm.so" >> /etc/asterisk/modules.conf
+/bin/echo "load => codec_alaw.so" >> /etc/asterisk/modules.conf
+/bin/echo "load => codec_dahdi.so" >> /etc/asterisk/modules.conf
+/bin/echo "load => codec_g722.so" >> /etc/asterisk/modules.conf
+/bin/echo "load => codec_g726.so" >> /etc/asterisk/modules.conf
+/bin/echo "load => codec_gsm.so" >> /etc/asterisk/modules.conf
+/bin/echo "load => codec_lpc10.so" >> /etc/asterisk/modules.conf
+/bin/echo "load => codec_resample.so" >> /etc/asterisk/modules.conf
+/bin/echo "load => codec_speex.so" >> /etc/asterisk/modules.conf
+/bin/echo "load => codec_ulaw.so" >> /etc/asterisk/modules.conf
+
+echo "Done."
