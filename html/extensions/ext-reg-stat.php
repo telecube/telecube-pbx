445f392112f490cb2b3e52693ee3f0358c73f645
telecube <john@telecube.com.au>
2016-01-11 23:17:36 +0700

    tidying up

--- html/extensions/ext-reg-stat.php
+++ html/extensions/ext-reg-stat.php
@@ -8,16 +8,8 @@ $res = $Db->pdo_query($q, array(), $dbPDO);
 $j = count($res);
 for($i=0;$i<$j;$i++) { 
 	$ext_stat["exts"][] = $res[$i]['name'];
-	//if($res[$i]['port'] > 0 && $res[$i]['regseconds'] > date("U")){
-//	if($Asterisk->ext_status($res[$i]['name']) > 0){
-		//$ext_stat[$res[$i]['name']] = "yes";
-		$ext_stat["data"][$res[$i]['name']] = $res[$i]['register_status'];
-//	}else{
-		//$ext_stat[$res[$i]['name']] = "no";
-//		$ext_stat["data"][$res[$i]['name']] = "no";
-//	}
+	$ext_stat["data"][$res[$i]['name']] = $res[$i]['register_status'];
 }
 
 echo json_encode($ext_stat);
-
 ?>
