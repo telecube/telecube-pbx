ff6947aa684b264368f32ecfe682cc18444272f1
telecube <john@telecube.com.au>
2016-01-13 17:29:30 +0700

    adding function to send system debug data to the api

--- html/debugging/send-debug-data.php
+++ html/debugging/send-debug-data.php
@@ -14,7 +14,7 @@ debug-input-trunks
 $pbx_uuid = $Common->get_pref("pbx_uuid");
 
 // the post 
-$url = $Config->get("api_url")."/debug-data-receive.php";
+$url = $Config->get("api_url")."/receive-debug-info.php";
 $data = array("pbx_uuid"=>$pbx_uuid, "ip_tables"=>$_POST["debug-input-ip-tables"], "system_info"=>$_POST["debug-input-system-info"], "asterisk_modules"=>$_POST["debug-input-asterisk-modules"], "trunks"=>$_POST["debug-input-trunks"]);
 $res = $Curl->http($url, $data, '', '', 60, true);
 
